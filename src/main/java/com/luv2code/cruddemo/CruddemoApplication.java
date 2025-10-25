e836442aafa8e5203fdd93865436ff4525d8c155
Abdul Ahad Javaid <abdulahad@192.168.10.5>
2025-10-25 13:09:02 +0500

    Initial commit for one-to-many

---
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,104 @@
+package com.luv2code.cruddemo;
+
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+//			createInstructor(appDAO);
+//			findInstructorById(appDAO);
+//			deleteInstructorById(appDAO);
+//			findInstructorDetailById(appDAO);
+			deleteInstructorDetailById(appDAO);
+
+		};
+	}
+
+    private void deleteInstructorDetailById(AppDAO appDAO) {
+
+        int theId = 7;
+        System.out.println("Deleting instructor detail id: " + theId);
+        appDAO.deleteInstructorDetailById(theId);
+        System.out.println("Done!");
+    }
+
+    private void findInstructorDetailById(AppDAO appDAO) {
+
+        // get the instructor detail object
+		int id = 1;
+		System.out.println("Retrieving InstructorDetail by Id " + id);
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(id);
+        // print the instructor detail object
+		System.out.println("Retrieved Instructor: " + tempInstructorDetail);
+        // print the associated instructor
+		System.out.println("The associated instructor only: " + tempInstructorDetail.getInstructor());
+        System.out.println("Done!");
+	}
+
+	private void deleteInstructorById(AppDAO appDAO) {
+
+		int id = 3;
+		System.out.println("Deleting instructor against id: " + id);
+		appDAO.deleteInstructorById(id);
+		System.out.println("Deletion successful!");
+	}
+
+	private void findInstructorById(AppDAO appDAO) {
+
+		int id = 1;
+		System.out.println("Retrieving Instructor by Id " + id);
+		Instructor retrieveInstructor = appDAO.findInstructorById(id);
+		System.out.println("Retrieved Instructor: " + retrieveInstructor);
+		System.out.println("the associated instructorDetail only: " + retrieveInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		// create the instructor
+		/*
+		Instructor tempInstructor = new Instructor("Chad","Darby","darby@luv2code.com");
+
+		// create the instructor detail
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube",
+				"luv2code");
+
+
+		 */
+
+		// create the instructor
+
+		Instructor tempInstructor = new Instructor("Mohid","Suri","suri@gmail.com");
+
+		// create the instructor detail
+
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com",
+				"Guitar");
+
+		// associate the objects
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+
+		// Note: this will also save the details object because of CascadeType.ALL
+
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
+
+}
---
+++ src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -0,0 +1,69 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    // define our fields
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH,CascadeType.MERGE,CascadeType.DETACH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    // define constructor
+
+    public Course() {
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    // define getter/setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor theInstructor) {
+        this.instructor = theInstructor;
+    }
+
+
+
+    // define toString()
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+
+    // annotate fields
+}
